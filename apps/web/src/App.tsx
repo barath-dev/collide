874530dd7ea3462ba9b4427da7213275ebaa9c2c
barathwaj <barathwaj1153@gmail.com>
2024-05-01 23:48:59 +0530

    added animation

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,20 @@
 
 import './App.css'
+import Introanimation from './components/Animation/Introanimation'
 import Waitlist from './components/Waitlist/Waitlist'
 
 function App() {
 
   return (
-    <div className="App">
-      <Waitlist/>
+    <>
+    <div className='ani'>
+    <Introanimation />
     </div>
+     <div className="App">
+     <Waitlist/>
+    </div>
+    </>
+   
   )
 }
 
---
+++ apps/web/src/components/Animation/Introanimation.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Introanimation.css'
+
+export default function Introanimation() {
+  return (
+    <div className="preloaderContainer">
+       <div className="leftCircle"></div>
+       <div className="rightCircle"></div>
+    </div>
+  )
+}
